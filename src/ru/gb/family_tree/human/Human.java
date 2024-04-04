a49ff9665206a4f5844112084dbffa2f14cec5b1
Saidjalol Sodikov <ssodikovu@gmail.com>
2024-04-04 23:55:13 +0500

    Доведено до уровня Семинара 2. Всё сделано как в семинаре, но без статичного метода тестового дерева. Данные для теста, загружаются из файла "tree.sav".

--- src/ru/gb/family_tree/human/Human.java
+++ src/ru/gb/family_tree/human/Human.java
@@ -1,10 +1,11 @@
 package ru.gb.family_tree.human;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Human {
+public class Human implements Serializable {
     private String name;
     private LocalDate dob, dod;
     private Gender gender;
--- src/ru/gb/family_tree/writer/Writable.java
+++ src/ru/gb/family_tree/writer/Writable.java
@@ -1,11 +1,8 @@
 package ru.gb.family_tree.writer;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import ru.gb.family_tree.tree.Tree;
+import java.io.Serializable;
 
 public interface Writable {
-    void saveTree(Tree tree) throws IOException;
-    void readTree(Tree tree) throws FileNotFoundException, IOException, ClassNotFoundException;
+    boolean save(Serializable serializable, String filePath);
+    Object read(String filePath);
 }
